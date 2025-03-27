53293b6638d4a92984a11def48893d2e8554c178
Vegar Lee Berentsen <designr.pros>
2025-03-27 04:42:12 +0100

    hydration issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,16 @@
-"use client"
-
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import styled from 'styled-components';
 import "./globals.css";
 import Toolbar from "./components/Toolbar";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-const StyledBody = styled.body`
-  background-color: #4b6079;
-  margin: 0;
-  min-height: 100vh;
-  overflow-y: scroll;
-  display: flex;
-  flex-direction: column;
-`;
+export const metadata: Metadata = {
+  title: "Tjenestedesign og PRIMA - AKTHE",
+  description: "Tjenestedesign og PRIMA-aktiviteter fra AKTHE.",
+};
 
 export default function RootLayout({
   children,
@@ -39,10 +32,10 @@ export default function RootLayout({
           }}
         />
       </head>
-      <StyledBody className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`styled-body ${geistSans.variable} ${geistMono.variable}`}>
         <Toolbar />
         {children}
-      </StyledBody>
+      </body>
     </html>
   );
 }
